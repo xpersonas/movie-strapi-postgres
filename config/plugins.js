f1d92a519eec7c76b5026f3a8b118fd4c68419ef
Justin Neel <justin.neel@gmail.com>
2025-12-28 12:33:33 -0600

    amazon s3 config

--- config/plugins.js
+++ config/plugins.js
@@ -19,5 +19,28 @@ module.exports = {
           }
         }
       }
-    }
+    },
+    upload: {
+      config: {
+        provider: 'aws-s3',
+        providerOptions: {
+          basePath: process.env.AWS_BASE_PATH || 'uploads',
+          s3Options: {
+            credentials: {
+              accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+              secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+            },
+            region: process.env.AWS_REGION,
+            params: {
+              Bucket: process.env.AWS_BUCKET_NAME,
+            },
+          },
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
   };
