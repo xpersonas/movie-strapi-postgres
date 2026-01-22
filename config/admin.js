c7ca18db59d4d34e85adfe3fa286ec5c9240ba21
Justin Neel <justin.neel@gmail.com>
2026-01-21 20:52:33 -0600

    updating sessions to 30d

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,9 @@
 module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'b4dbad35-6caa-4f84-be56-90d0d3bc2fdf'),
+    options: {
+      expiresIn: '30d',
+    },
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
